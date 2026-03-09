bafdf643ee2909eb9c6359a4e68f23a7d0ccb038
Flam64 <celio.mozes.rocha@gmail.com>
2026-03-09 19:19:00 +0100

    feat: update main page app

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
-//import Projects from "@/sections/Projects";
 //import Contact from "@/sections/Contact";
 import Footer from "@/components/layout/Footer";
+import Projects from "@/sections/Projects";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Projects />
       <Footer />
     </main>
   );
