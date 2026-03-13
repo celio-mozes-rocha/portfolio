202ca18c4452d14c28fb53b5d86934a098dd3bef
Flam64 <celio.mozes.rocha@gmail.com>
2026-03-13 22:03:47 +0100

    feat(experience): add experience cards and complete experiences page with real content

--- components/sections/experiences/ExperienceCard.tsx
+++ components/sections/experiences/ExperienceCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Experience } from "@/data/experiencesData";
+import type { ExperiencesType } from "@/data/experiencesData";
 
-export default function ExperienceCard({ exp }: { exp: Experience }) {
+export default function ExperienceCard({ exp }: { exp: ExperiencesType }) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
@@ -16,7 +16,40 @@ export default function ExperienceCard({ exp }: { exp: Experience }) {
         <h3 className="font-semibold text-lg">
           {exp.title} {exp.company ? `– ${exp.company}` : ""}
         </h3>
-        <p className="text-gray-400 leading-relaxed space-y-4 text-[18px]" >{exp.description}</p>
+        <p className="text-gray-400 text-[18px]" >{exp.description}</p>
+
+
+        {exp.type === "job" && (
+          <h3 className="list-disc pl-5 space-y-1 text-sm text-slate-400">
+            <ul>
+              {exp.responsibilities.map((r, i) => (
+                <li className="relative pl-3 before:content-['⬩'] before:absolute before:left-0 before:text-sky-400 text-[16px]" key={i}>{r}</li>
+              ))}
+            </ul>
+          </h3>
+        )}
+
+        {exp.type === "education" && (
+          <h3 className="list-disc pl-5 space-y-1 text-sm text-slate-400">
+            <ul>
+              {exp.learnings.map((l, i) => (
+                <li className="relative pl-3 before:content-['⬩'] before:absolute before:left-0 before:text-sky-400 text-[16px]" key={i}>{l}</li>
+              ))}
+            </ul>
+          </h3>
+        )}
+
+        {exp.type === "project" && (
+          <h3 className="list-disc pl-5 space-y-1 text-sm text-slate-400">
+            <ul>
+              {exp.collaboration?.map((c, i) => (
+                <li className="relative pl-3 before:content-['⬩'] before:absolute before:left-0 before:text-sky-400 text-[16px]" key={i}>{c}</li>
+              ))}
+            </ul>
+          </h3>
+        )}
+
+
         {exp.tech && (
           <div className="flex flex-wrap gap-2 mt-1 text-xs">
             {exp.tech.map((t, i) => (
